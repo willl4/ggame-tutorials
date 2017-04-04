e19c5d8b0f4be67af8f6157c36785e85d97a1611
willl4 <williamlaycock@hanovernorwichschools.org>
2017-04-04 14:21:52 -0400

    Updated from Brython Server: 4/4/2017 2:21:52 PM

--- tutorial4.py
+++ tutorial4.py
@@ -4,11 +4,17 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 # Background
-black = Color(0, 1)
-noline = LineStyle(0, black)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
-bg = Sprite(bg_asset, (0,0))
 
 
-myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
